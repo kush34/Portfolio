265078301c3fdcf7cd81e739e88835faaa4cbf70
kush34 <chattkush34@gmail.com>
2025-12-28 15:02:36 +0530

    fixed company image

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -5,14 +5,14 @@ const ExperienceCard = ({ name, position, time, points, link, imageLink, altImag
     return (
         <div className='z-10 flex flex-col gap-5'>
             <div className='flex justify-start items-center gap-10'>
-                <span className='bg-zinc-100 rounded'>
-                    <img src={imageLink} alt={altImage} className='p-5 size-20 rounded' />
+                <span className='bg-zinc-100 rounded flex items-center'>
+                    <img src={imageLink} alt={altImage} className='p-2 md:p-5 size-15 md:size-20 rounded' />
                 </span>
                 <div className='flex flex-col'>
-                    <span className='text-xl font-medium'>{name} </span>
-                    <span className=''>{position} </span>
+                    <span className='text-md xl:text-xl font-medium'>{name} </span>
+                    <span className='text-zinc-600 text-sm xl:text-xl'>{position} </span>
                 </div>
-                <div className='flex flex-col items-center text-zinc-500'>
+                <div className='hidden md:flex flex-col items-center text-zinc-500'>
                     {link && <a className='flex gap-2 items-center' rel="noopener noreferrer" aria-label={`Company site link for ${name || 'Company'}`} target={'_blank'} href={`${link}`}><GoArrowUpRight size={24} /></a>}
                     <span className='text-sm font-light '>{time}</span>
 
