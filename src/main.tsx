5e7f55356ed983686eccff48d1abbc912d8f2e5a
kush34 <chattkush34@gmail.com>
2026-06-24 14:10:12 +0530

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,30 @@
 import * as ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from "./App.jsx";
-import "./index.css"
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import Terms from "./pages/Terms";
-import BlogPage from "./pages/BlogPage.js";
-import Layout from "./layout/main.js";
+import { lazy, Suspense } from "react";
+
+import "./index.css";
+
+const App = lazy(() => import("./App.jsx"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
+const Terms = lazy(() => import("./pages/Terms"));
+const BlogPage = lazy(() => import("./pages/BlogPage.js"));
+const Layout = lazy(() => import("./layout/main.js"));
 
 const root = document.getElementById("root");
 
 if (root) {
   ReactDOM.createRoot(root).render(
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<App />} />
-          <Route path="/blog/:slug" element={<BlogPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<Terms />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<App />} />
+            <Route path="/blog/:slug" element={<BlogPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<Terms />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-
   );
-} 
+}
